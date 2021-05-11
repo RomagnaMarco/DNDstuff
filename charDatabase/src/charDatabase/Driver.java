eea1132a222b517d4ed61ab2f9be1ce5bebd4827
Marco <Marco@DESKTOP-MJ1BTIP>
2021-05-11 16:52:38 -0700

    Updated mysql connector to new version

--- charDatabase/src/charDatabase/Driver.java
+++ charDatabase/src/charDatabase/Driver.java
@@ -1,9 +1,19 @@
 package charDatabase;
 
+import java.sql.*;
+
 public class Driver {
 	
 	public static void main(String[] args) {
 		
+		
+		try {
+			
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 
 }
