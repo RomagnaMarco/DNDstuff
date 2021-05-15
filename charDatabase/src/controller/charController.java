1c4d5436c6ff7b5785df9218dcb10449584060d1
Marco <Marco@DESKTOP-MJ1BTIP>
2021-05-15 03:43:51 -0700

    Starting MVC implementation of Controller

---
+++ charDatabase/src/controller/charController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javax.swing.JFrame;
+import view.*;
+
+
+/**
+ * Controller class of MVC arch.
+ * @author Marco
+ *
+ */
+public class charController {
+
+		
+		private static JFrame frame;
+		
+		/**
+		 * constructor for character controller
+		 * @param frame used to draw the pages
+		 */
+		public charController(JFrame frame) 
+		{
+			this.frame = frame;
+		}
+		
+		
+		public static void main(String[] args) {
+			JFrame frame = new JFrame();
+			
+	}
+
+}
