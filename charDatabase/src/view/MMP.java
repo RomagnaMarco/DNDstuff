7a3f4a4c8476aad5ed87340ad1b72ebf99a808f2
Marco <Marco@DESKTOP-MJ1BTIP>
2021-05-20 14:53:17 -0700

    Cleaned up the problem on diceRolls GUI
    cleaned up some code

--- charDatabase/src/view/MMP.java
+++ charDatabase/src/view/MMP.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -15,6 +16,7 @@ import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
 
 /**
  * Main Menu Page.
@@ -54,36 +56,21 @@ public class MMP {
 		viewBP.setSize(455,700);
 		frame.add(viewBP, BorderLayout.NORTH);
 		//view button config
-		viewB.setPreferredSize(new Dimension(400, 200));
-		viewB.setOpaque(true);
-		viewB.setBackground(new Color(41, 142, 208));
-		viewB.setForeground(Color.white);
-		viewB.setBorderPainted(false);
-		viewB.setFont(new Font("Arial", Font.BOLD, 40));
+		basicButtonSetup(viewB);
 		viewBP.add(viewB);
 		
 		JPanel ccBP = new JPanel();
 		ccBP.setSize(455,700);
 		frame.add(ccBP, BorderLayout.CENTER);
 		//create character button config
-		ccB.setPreferredSize(new Dimension(400, 200));
-		ccB.setOpaque(true);
-		ccB.setBackground(new Color(41, 142, 208));
-		ccB.setForeground(Color.white);
-		ccB.setBorderPainted(false);
-		ccB.setFont(new Font("Arial", Font.BOLD, 40));
+		basicButtonSetup(ccB);
 		ccBP.add(ccB);
 		
 		JPanel diceBP = new JPanel();
 		ccBP.setSize(455,700);
 		frame.add(diceBP, BorderLayout.SOUTH);
 		//Dice Roller button config
-		diceB.setPreferredSize(new Dimension(400, 200));
-		diceB.setOpaque(true);
-		diceB.setBackground(new Color(41, 142, 208));
-		diceB.setForeground(Color.white);
-		diceB.setBorderPainted(false);
-		diceB.setFont(new Font("Arial", Font.BOLD, 40));
+		basicButtonSetup(diceB);
 		diceBP.add(diceB);
 		
 		//frame finishing touches
@@ -92,7 +79,7 @@ public class MMP {
 		frame.repaint();
 		frame.revalidate();
 		
-		popBool = false;
+		popBool = false; //needed to determine whether or not popFrame has been opened
 	}
 	
 	/**
@@ -151,14 +138,15 @@ public class MMP {
 		
 		//dropdown setup
 		
-		 Integer list[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		Integer list[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		Integer list2[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 		JComboBox<Integer> DDd4 = new JComboBox<>(list); //drop down d4
 		JComboBox<Integer> DDd6 = new JComboBox<>(list); //drop down d6
 		JComboBox<Integer> DDd8 = new JComboBox<>(list); //drop down d8
 		JComboBox<Integer> DDd10 = new JComboBox<>(list); //drop down d10
 		JComboBox<Integer> DDd12 = new JComboBox<>(list); //drop down d12
 		JComboBox<Integer> DDd20 = new JComboBox<>(list); //drop down d20
-		JComboBox<Integer> DDbonus = new JComboBox<>(list); //drop down bonus
+		JComboBox<Integer> DDbonus = new JComboBox<>(list2); //drop down bonus
 		dicePanel.add(DDd4);dicePanel.add(d4);
 		dicePanel.add(DDd6);dicePanel.add(d6);
 		dicePanel.add(DDd8);dicePanel.add(d8);
@@ -169,14 +157,21 @@ public class MMP {
 		
 		//display setup
 		JPanel displayP = new JPanel();
-		GridLayout subLayout2 = new GridLayout(1,7);
+		displayP.setBorder(new LineBorder(Color.LIGHT_GRAY,3));
+		GridLayout subLayout2 = new GridLayout(8,1);
 		displayP.setLayout(subLayout2);
 		ArrayList<JTextArea> arr = new ArrayList<>();
 		//adds new text areas in arrayList
-		for(int i = 0; i < 7; i++) {//adds 8 text areas
+		for(int i = 0; i < 8; i++) {//adds 9 text areas
 			arr.add(new JTextArea());
 		}
+		//clarify most recent roll
+		JTextArea recentRoll = arr.get(0);
+		recentRoll.setBackground(Color.LIGHT_GRAY);
+		recentRoll.setFont(recentRoll.getFont().deriveFont(Font.BOLD, 14f));
+		recentRoll.append("Result: ");
 		
+		//make it so the user can't edit results of rolls
 		for (int i =0; i < arr.size(); i++){ //Sets all to non-editable and adds them
 			arr.get(i).setEditable(false);
 			displayP.add(arr.get(i));
@@ -202,12 +197,24 @@ public class MMP {
 		popFrame.add(panel);
 		popFrame.pack();
 		
-		JLayeredPane popup = new JLayeredPane();
-		popFrame.getContentPane().add(popup);
 		popFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		popFrame.setSize(300,390);
 		popFrame.setResizable(false);
 		popFrame.setVisible(true);
+	}
+	
+	/**
+	 * takes in JButton. Setups up basic appearance
+	 * @param b
+	 */
+	public void basicButtonSetup(JButton b)
+	{
+		b.setPreferredSize(new Dimension(400, 200));
+		b.setOpaque(true);
+		b.setBackground(new Color(41, 142, 208));
+		b.setForeground(Color.white);
+		b.setBorderPainted(false);
+		b.setFont(new Font("Arial", Font.BOLD, 40));
 	}
 	
 }
