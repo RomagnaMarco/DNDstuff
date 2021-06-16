80877e5f5bcdaff880d3737b2ceb8fb81fbcadb4
Marco <Marco@DESKTOP-MJ1BTIP>
2021-06-15 18:25:41 -0700

    Generated Some basic getters & setters + javadoc for PointBuyPage(view)
    covered javadocs for the main ones but left out certain ones as I might
    change those still.
    Also moved Initialization to new PointBuyPage Constructor
    Made variables private.

--- charDatabase/src/model/PointBuyPage.java
+++ charDatabase/src/model/PointBuyPage.java
@@ -1,30 +1,201 @@
 package model;
 
 public class PointBuyPage {
-	boolean cont = false; // cannot continue by default
-						  // need to make sure the points are all spent
 	
-	int selected = 27; // default point buy starts here
-					   // Can change on hitting confirm button after messing
-					   // with the JCombo box (drop down)
-	int pointsLeft = selected; // points left starts at amt selected
+	private boolean cont;
+	private int selected;
+	private int pointsLeft;
+	
+	private int Str;
+	private int Dex;
+	private int Con;
+	private int Int;
+	private int Wis;
+	private int Cha;
+	
+	private int StrPoint;
+	private int DexPoint;
+	private int ConPoint;
+	private int IntPoint;
+	private int WisPoint;
+	private int ChaPoint;
+	
+	public PointBuyPage()
+	{
+
+		cont = false; // cannot continue by default
+							  // need to make sure the points are all spent
+		
+		selected = 27; // default point buy starts here
+						   // Can change on hitting confirm button after messing
+						   // with the JCombo box (drop down)
+		
+		pointsLeft = selected; // points left starts at amount selected
 							   // is modified when allocation starts
-							   // is modified when a new amt of points is selected.
+							   // is modified when a new amount of points is selected.
+		
+		//Stats / Scores / Ability Modifiers /Attributes default.
+		Str = 8;
+		Dex = 8;
+		Con = 8;
+		Int = 8;
+		Wis = 8;
+		Cha = 8;
+		
+		//points spent on each. Changes when their respective stat gets lowered or raised.
+		StrPoint = 0;
+		DexPoint = 0;
+		ConPoint = 0;
+		IntPoint = 0;
+		WisPoint = 0;
+		ChaPoint = 0; 
+	}
+
+	/**
+	 * For PointBuy Page in Model. 
+	 * @return if can continue away from this page via button push
+	 */
+	public boolean isCont() {
+		return cont;
+	}
+
+	/**
+	 * For PointBuy Page in Model. 
+	 * @param cont new boolean value for if you can continue
+	 */
+	public void setCont(boolean cont) {
+		this.cont = cont;
+	}
+
+	/**
+	 * For PointBuy Page in Model.
+	 * @return value selected by drop Down in CCPB (View)
+	 */
+	public int getSelected() {
+		return selected;
+	}
+
+	/**
+	 * For PointBuy Page in Model. 
+	 * Should be Accompanied by a change to the PointsLeft in both view and model
+	 * @param selected Upon selection of new value in the drop down menu in CCPB (view)
+	 */
+	public void setSelected(int selected) {
+		this.selected = selected;
+	}
 	
-	//Stats/Scores/ Ability Mods default.
-	int Str = 8;
-	int Dex = 8;
-	int Con = 8;
-	int Int = 8;
-	int Wis = 8;
-	int Cha = 8;
+	/**
+	 * For PointBuy Page in Model.
+	 * @return how many left the user has to spend on stats
+	 */
+	public int getPointsLeft() {
+		return pointsLeft;
+	}
+
+	/**
+	 * For PointBuy Page in Model.
+	 * @param pointsLeft changes the amount of points left to a certain Amount
+	 */
+	public void setPointsLeft(int pointsLeft) {
+		this.pointsLeft = pointsLeft;
+	}
+
 	
-	//points spent on each. Changes when their respective stat gets lowered or raised.
-	int StrPoint = 0;
-	int DexPoint = 0;
-	int ConPoint = 0;
-	int IntPoint = 0;
-	int WisPoint = 0;
-	int ChaPoint = 0; 
+	public int getStr() {
+		return Str;
+	}
+
+	
+	public void setStr(int str) {
+		Str = str;
+	}
+
+	public int getDex() {
+		return Dex;
+	}
+
+	public void setDex(int dex) {
+		Dex = dex;
+	}
+
+	public int getCon() {
+		return Con;
+	}
+
+	public void setCon(int con) {
+		Con = con;
+	}
+
+	public int getInt() {
+		return Int;
+	}
+
+	public void setInt(int i) {
+		Int = i;
+	}
+
+	public int getWis() {
+		return Wis;
+	}
+
+	public void setWis(int wis) {
+		Wis = wis;
+	}
+
+	public int getCha() {
+		return Cha;
+	}
+
+	public void setCha(int cha) {
+		Cha = cha;
+	}
+
+	public int getStrPoint() {
+		return StrPoint;
+	}
+
+	public void setStrPoint(int strPoint) {
+		StrPoint = strPoint;
+	}
+
+	public int getDexPoint() {
+		return DexPoint;
+	}
+
+	public void setDexPoint(int dexPoint) {
+		DexPoint = dexPoint;
+	}
+
+	public int getConPoint() {
+		return ConPoint;
+	}
+
+	public void setConPoint(int conPoint) {
+		ConPoint = conPoint;
+	}
+
+	public int getIntPoint() {
+		return IntPoint;
+	}
+
+	public void setIntPoint(int intPoint) {
+		IntPoint = intPoint;
+	}
+
+	public int getWisPoint() {
+		return WisPoint;
+	}
+
+	public void setWisPoint(int wisPoint) {
+		WisPoint = wisPoint;
+	}
+
+	public int getChaPoint() {
+		return ChaPoint;
+	}
+
+	public void setChaPoint(int chaPoint) {
+		ChaPoint = chaPoint;
+	}
 	
 }
